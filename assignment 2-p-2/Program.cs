a6b37c3e8d1035bf9297005cfac5cbc5ac06faa5
Sahithi-kasarla <sahithikasarla123@gmail.com>
2023-11-09 16:03:13 +0530

    assignment 2-p-2

---
+++ assignment 2-p-2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2_p_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for loop
+            Console.WriteLine("first 10 natural numbers are ");
+            for (int i= 0 ; i <= 10; i++)
+            {
+                
+                Console.WriteLine(i);
+            }
+
+            //while loop
+            int j = 2;
+            Console.WriteLine("first 10  even natural numbers are " + j);
+            while( j<=10)
+            {
+                Console.WriteLine(j);
+                j+=2;
+            }
+
+            //do-while loop
+            int k = 1;
+            Console.WriteLine("the first 10 odd natural numbers are ");
+
+            do
+            {
+                Console.WriteLine(k);
+                k += 2;
+            }
+            while (k <= 10);
+            
+             Console.ReadKey();
+        }
+       
+
+    }
+}
